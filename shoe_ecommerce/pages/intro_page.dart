363ba86513b6cf92352d040159d16a0d9ba79153
bishrut123 <104518942+bishrut123@users.noreply.github.com>
2024-05-11 09:25:29 +0545

    Add files via upload

---
+++ shoe_ecommerce/pages/intro_page.dart
@@ -0,0 +1,80 @@
+import 'package:e_commerce_app_2/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //children
+
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset('lib/images/logo.png', height: 240),
+              ),
+
+              const SizedBox(height: 25),
+
+              //title
+              const Text(
+                'Just do it',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+
+              //subtitle
+              const Text(
+                'Brand new sneakers and custom kicks made with premium quality',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 25),
+
+              //button
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: Center(
+                    child: Text(
+                      'Shop Now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ shoe_ecommerce/pages/shop_page.dart
@@ -0,0 +1,117 @@
+import 'package:e_commerce_app_2/components/shoe_tile.dart';
+import 'package:e_commerce_app_2/models/cart.dart';
+import 'package:e_commerce_app_2/models/shoe.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  //adding shoe to cart
+  void addShoeToCart(Shoe shoe) {
+    Provider.of<Cart>(context, listen: false).addItemToCart(shoe);
+
+    //alert user, shoe successfully added
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('successfully added'),
+        content: Text('check your cart'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Cart>(
+      builder: (context, value, child) => Column(
+        children: [
+          //search bar
+          Container(
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.circular(8)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Search',
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+                Icon(Icons.search),
+              ],
+            ),
+          ),
+
+          //message
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25.0),
+            child: Text(
+              'everyone flies.. some fly longer than others',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+          ),
+
+          //hot picks
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Hot picks 🔥',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                Text(
+                  'See all',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 10),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                Shoe shoe = value.getShoeList()[index];
+
+                //return the shoe
+                return ShoeTile(
+                  shoe: shoe,
+                  onTap: () => addShoeToCart(shoe),
+                );
+              },
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0, left: 25, right: 25),
+            child: Divider(
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
